b86296624654c1906c2b87adafe0a7d65d4c6ecd
perdub <81479253+lesh6295-png@users.noreply.github.com>
2023-05-26 15:58:03 +0000

    rework sources

---
+++ Sources.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using BooruSharp.Booru;
+using BooruSharp.Others;
+
+namespace rever{
+    public class ActiveSources{
+        public bool Pixiv {get;set;}
+        public bool Yandere {get;set;}
+        public bool Gelbooru {get;set;}
+        public bool DanbooruDonmai {get;set;}
+        public bool SankakuComplex {get;set;}
+        public bool Sakugabooru {get;set;}
+        public bool Lolibooru {get;set;}
+    }
+    public static class BoooruFactory{
+        public static List<ABooru> Build(this ActiveSources source, HttpClient client, string pixivrefresh){
+            List<ABooru> res = new();
+
+            if(source.DanbooruDonmai){
+                res.Add(new DanbooruDonmai());
+                res[^1].HttpClient = client;
+            }
+            if(source.Yandere){
+                res.Add(new Yandere());
+                res[^1].HttpClient = client;
+            }
+            if(source.Gelbooru){
+                res.Add(new Gelbooru());
+                res[^1].HttpClient = client;
+            }
+            if(source.Pixiv){
+                res.Add(new Pixiv());
+                ((Pixiv)res[^1]).LoginAsync(pixivrefresh).Wait();
+                res[^1].HttpClient = client;
+            }
+            if(source.SankakuComplex){
+                res.Add(new SankakuComplex());
+                res[^1].HttpClient = client;
+            }
+            if(source.Sakugabooru){
+                res.Add(new Sakugabooru());
+                res[^1].HttpClient = client;
+            }
+            if(source.Lolibooru){
+                res.Add(new Lolibooru());
+                res[^1].HttpClient = client;
+            }
+            
+
+            return res;
+        }
+    }
+}
